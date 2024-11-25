0b898ffceec445e463ff2554990a3dc21797df6d
vodaiphuoc <phuocvodn98gmail.com>
2024-11-25 04:28:29 +0000

    test triton

--- src/server/models/Facenet_pytorch/fine_tuning.py
+++ src/server/models/Facenet_pytorch/fine_tuning.py
@@ -96,7 +96,7 @@ class FineTuner(object):
 
 		# import torch._dynamo
 		# torch._dynamo.reset()
-		import triton
+		# import triton
 		
 		torch.cuda.set_device(rank)
 		model = torch.compile(model.to(rank), backend = 'cudagraphs')
