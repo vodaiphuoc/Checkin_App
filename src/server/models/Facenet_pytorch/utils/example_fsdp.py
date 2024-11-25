2c0332a6ff41df42b213118f8fae6fce381a63b5
vodaiphuoc <phuocvodn98gmail.com>
2024-11-25 07:48:24 +0000

    test torch compile

--- src/server/models/Facenet_pytorch/utils/example_fsdp.py
+++ src/server/models/Facenet_pytorch/utils/example_fsdp.py
@@ -1,18 +1,19 @@
 
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 import torch.distributed as dist
+from src.server.models.Facenet_pytorch.inception_resnet_v1 import InceptionResnetV1
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch
 import torch.multiprocessing as mp
 import os
 
-class MyModule(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.lin = torch.nn.Conv2d(3, 32, kernel_size = (3,3), stride = 1)
+# class MyModule(torch.nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         self.lin = torch.nn.Conv2d(3, 32, kernel_size = (3,3), stride = 1)
 
-    def forward(self, x):
-        return torch.mean(torch.nn.functional.relu(self.lin(x)))
+#     def forward(self, x):
+#         return torch.mean(torch.nn.functional.relu(self.lin(x)))
 
 def setup(rank:int, world_size:int):
     os.environ['MASTER_ADDR'] = 'localhost'
@@ -26,9 +27,15 @@ def cleanup():
 
 def main_mapping(rank, world_size, batch):
         setup(rank, world_size)
-        mod = MyModule().to(rank)
+        mod = InceptionResnetV1(pretrained = 'casia-webface', 
+								classify=False,
+								num_classes=None, 
+								dropout_prob=0.6,
+								device = rank,
+								pretrained_weight_dir = '/kaggle/input/massive-faces'
+        					).to(rank)
         opt_mod = torch.compile(mod)
         opt_mod = FSDP(opt_mod, use_orig_params = True)
-        t = torch.randn(1,3,60,60).to(rank)
+        t = torch.randn(2,3,160,160).to(rank)
         print(opt_mod(t))
         cleanup()
